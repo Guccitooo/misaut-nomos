f40ff7ac40f3ec471acb2a9c7eaacf8955939d65
Ilias Ahmed Benchellal <bigseo.online.com@gmail.com>
2025-11-23 14:01:44 +0000

    File changes

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -739,7 +739,7 @@ export default function SearchPage() {
             </Card>
           )}
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             <AnimatePresence>
               {filteredProfiles.map((profile) => (
                 <motion.div
