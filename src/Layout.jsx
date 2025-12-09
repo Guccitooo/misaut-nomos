a49d5f63610fb3f10b1bfc748231207febb6eed1
Ilias Ahmed Benchellal <bigseo.online.com@gmail.com>
2025-12-09 08:49:15 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -38,7 +38,7 @@ import { useLanguage, LanguageProvider } from "@/components/ui/LanguageSwitcher"
 import ScrollToTop from "@/components/ui/ScrollToTop";
 import NotificationCenter from "@/components/notifications/NotificationCenter";
 import OptimizedImage from "@/components/ui/OptimizedImage";
-import AIAssistantButton from "@/components/chat/AIAssistantButton";
+// import AIAssistantButton from "@/components/chat/AIAssistantButton";
 import WebsiteSchema from "@/components/seo/WebsiteSchema";
 
 const LOGO_URL = 'https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/690076ad86e673c796768de5/47f6f564f_ChatGPTImage13nov202511_25_45.png';
@@ -1195,7 +1195,7 @@ const LayoutContent = React.memo(function LayoutContent({ children, currentPageN
           </div>
 
           <CookieBanner />
-          <AIAssistantButton />
+          {/* <AIAssistantButton /> */}
           <WebsiteSchema />
           </div>
           </SidebarProvider>
